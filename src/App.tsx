cdc47470d88476e6f6dcfe3dee4870ae999b14d4
linmiaomiao1998 <994717247@qq.com>
2021-03-17 10:04:30 +0800

    完成基本路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components'
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav'
+const Wrapper=styled.div`
+height: 100vh;
+  display:flex;
+  flex-direction: column;
+  `;
 
-const  Button =styled.button`
-color:red;
-`;
+  const Main=styled.div`
+  flex-grow: 1;
+  overflow: auto;
+  `;
+ 
 
 function App() {
   return (
-    <div> 
-      <Button> hi </Button>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+        </Switch>
+      </Main>
+      <Nav />
+         
+        </Wrapper>
+    </Router>
   );
 }
+function NoMatch(){
+  return ( 
+    <div>输错地址</div>
+  )
+}
 
+function Tags() {
+  return <h2>标签</h2>;
+}
+
+function Money() {
+  return <h2>记账</h2>;
+}
+
+function Statistics() {
+  return <h2>统计</h2>;
+}
 export default App;
